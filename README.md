639bf8816f4c9c2d074694806fffab7fa983fb29
Tien Le-Quang <quangtiencs@gmail.com>
2025-03-10 01:07:27 +0700

    add chapter 3

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Keras3-Inside-Deep-Learning
 
 Keras 3 version of https://github.com/EdwardRaff/Inside-Deep-Learning
+
+To open this project using the uv Python package manager, execute the following command: 
+```shell
+uv run jupyter lab
+```
--- utils.py
+++ utils.py
@@ -3,16 +3,23 @@ from keras import metrics
 
 
 class CustomEvalDataset(callbacks.Callback):
-    def __init__(self, x, y, metrics=metrics.SparseCategoricalAccuracy()):
+    def __init__(
+        self, x=None, y=None, datasets=None, metrics=metrics.SparseCategoricalAccuracy()
+    ):
         super().__init__()
         self.x = x
         self.y_true = y
+        self.datasets = datasets
         self.metrics = metrics
         self.list_metrics = []
 
     def on_epoch_end(self, epoch, logs=None):
         self.metrics.reset_state()
-        y_pred = self.model.predict(self.x, verbose=0)
-        self.metrics.update_state(self.y_true, y_pred)
-        result = float(self.metrics.result())
-        self.list_metrics.append(result)
+        if self.datasets is None:
+            y_pred = self.model.predict(self.x, verbose=0)
+            self.metrics.update_state(self.y_true, y_pred)
+            result = float(self.metrics.result())
+            self.list_metrics.append(result)
+        else:
+            loss, metric = self.model.evaluate(self.datasets, verbose=0)
+            self.list_metrics.append(metric)
